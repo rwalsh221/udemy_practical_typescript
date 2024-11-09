7647ac6dcae42526eca4b21eae69e9fa015204cd
Richard Walsh <richwalsh2@googlemail.com>
2024-11-09 21:44:24 +0000

    section 3 lesson 8

--- practical_typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical_typescript/01-typescript-tutorial/src/tutorial.ts
@@ -1,1 +1,12 @@
-console.log('hello');
+let name: string = 'john';
+name = 'chloe';
+name = name.toUpperCase();
+console.log(name);
+
+let amount: number = 20;
+amount = 12 - 1;
+// amount = 'hello'; not number error
+
+let bool: boolean = true;
+bool = false;
+// bool = 'hello'; not bool error
