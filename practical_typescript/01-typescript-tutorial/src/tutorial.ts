5b6c63c207bc4a94b1337ea85dcebed92c13c131
Richard Walsh <richwalsh2@googlemail.com>
2024-11-10 21:19:14 +0000

    lesson 15

--- practical_typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical_typescript/01-typescript-tutorial/src/tutorial.ts
@@ -10,3 +10,39 @@ amount = 12 - 1;
 let bool: boolean = true;
 bool = false;
 // bool = 'hello'; not bool error
+
+let test = 'test';
+// test = 2; inference not string error
+
+// ***** LESSON 12 UNION TYPE *****
+
+let tax: number | string = 10;
+tax = '9';
+
+let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+requestStatus = 'success';
+
+// ***** LESSON 13 ANY TYPE *****
+
+let notSure: any = 4;
+notSure = '4';
+notSure = true;
+
+let anyValue; // infers any type
+anyValue = 10;
+anyValue = '10';
+
+// ***** LESSON 14 *****
+
+const books = ['1984', 'Brave New World', 'Fahrenheit 451'];
+
+let foundBook: string | undefined;
+
+for (let book of books) {
+  if (book === '1984') {
+    foundBook = book;
+  }
+  break;
+}
+
+console.log(foundBook?.length);
