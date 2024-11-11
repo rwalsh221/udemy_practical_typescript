63f05f2171c2a94f130df840d521f1bb6432eb7e
Richard Walsh <richwalsh2@googlemail.com>
2024-11-11 20:36:47 +0000

    section 3 lesson 19

--- practical_typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical_typescript/01-typescript-tutorial/src/tutorial.ts
@@ -46,3 +46,36 @@ for (let book of books) {
 }
 
 console.log(foundBook?.length);
+
+// ***** LESSON 16 ARRAYS *****
+
+let prices: number[] = [100, 2, 4];
+
+prices.push(9);
+
+let fruit: string[] = ['apple', 'orange'];
+
+// let randomValues: [] = [0, 9]; error expects empty array
+
+let names = ['peter', 'susan', 1];
+
+names.push(9);
+let array: (string | boolean)[] = ['apple', true];
+
+// ***** LESSON 18 OBJECT FUNDAMENTALS *****
+
+let car: { brand: string; year: number } = {
+  brand: 'ford',
+  year: 2020,
+};
+
+let book = { title: 'book', cost: 20 };
+let pen = { title: 'pen', cost: 20 };
+let notebook = { title: 'notebook' };
+
+let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
+
+// items[0].title = 'book2' readonly error
+book.title = 'book2'; // can still change original object
+
+// ***** LESSON 20 FUNCTIONS *****
