8ca30efa8017460db2e4af6171485071d42fc0db
Richard Walsh <richwalsh2@googlemail.com>
2024-11-12 21:23:50 +0000

    section 3 lesson 28

--- practical_typescript/01-typescript-tutorial/src/tutorial.ts
+++ practical_typescript/01-typescript-tutorial/src/tutorial.ts
@@ -79,3 +79,108 @@ let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
 book.title = 'book2'; // can still change original object
 
 // ***** LESSON 20 FUNCTIONS *****
+
+const sayHi = (name: string) => {
+  return `hi ${name}`;
+};
+
+const useSayHi = sayHi('paul');
+useSayHi.toLocaleUpperCase();
+
+console.log(sayHi('john'));
+
+// ***** LESSON 21 FUNCTIONS - RETURNS *****
+
+const calculateDiscount = (price: number): number => {
+  const hasDiscount = true;
+
+  if (hasDiscount) {
+    return price;
+    // return 'disocunt applied';
+  }
+
+  return price * 0.9;
+};
+
+const finalPrice = calculateDiscount(200);
+
+const namesArray: string[] = ['john', 'paul'];
+
+const checkName = (name: string, namesArray: string[]): boolean => {
+  let nameFound = false;
+  for (let nameA of namesArray) {
+    if (nameA === name) {
+      nameFound = true;
+    } else {
+      nameFound = false;
+    }
+  }
+  return nameFound;
+};
+
+console.log(checkName('john', namesArray));
+
+// ***** LESSON 24 OPTIONAL PARAMETERS *****
+
+const calcPrice = (price: number, discount?: number) => {
+  return price - (discount || 0);
+};
+
+let priceAfterDiscount = calcPrice(100, 20);
+
+// ***** LESSON 25 DEFAULT PARAMETER *****
+
+let calcScore = (initialScore: number, penaltyPoints: number = 0) => {
+  return initialScore - penaltyPoints;
+};
+
+let scoreAfterPenalty = calcScore(100, 20);
+let scoreWithoutPenalty = calcScore(100);
+
+// ***** LESSON 26 FUNCTIONS - REST PARAMETER *****
+
+const sum = (message: string, ...numbers: number[]) => {
+  const doubled = numbers.map((num) => num * 2);
+
+  let total = numbers.reduce((prev, current) => {
+    return prev + current;
+  }, 0);
+  return `${message} ${total}`;
+};
+
+let sumResult = sum('the total is', 1, 2, 3, 4, 4, 5, 6, 7);
+
+// ***** LESSON 27 FUNCTIONS - VOID KEYWORD *****
+
+const log = (message: string) => {
+  console.log(message);
+};
+
+log('hllo');
+
+// ***** LESSON 28 FUNCTIONS - TYPE GUARDS *****
+
+const processInput = (input: string | number): void => {
+  if (typeof input === 'number') {
+    console.log(input * 2);
+  } else {
+    console.log(input.toUpperCase());
+  }
+};
+
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
+// ***** LESSON 20 FUNCTIONS *****
