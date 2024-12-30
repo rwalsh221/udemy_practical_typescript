c4dad1246d1c52d1a59996554783ff8392bf60f7
Richard Walsh <richwalsh2@gmail.com>
2024-12-30 19:34:45 +0000

    section 5 lesson 140

--- practical_typescript/03-store-project/src/pages/HomeLayout.tsx
+++ practical_typescript/03-store-project/src/pages/HomeLayout.tsx
@@ -1,11 +1,11 @@
-import { Header } from "@/components";
+import { Header, Navbar } from "@/components";
 import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
-      <nav>navbar</nav>
+      <Navbar />
       <div className="align-element py-20">
         <Outlet />
       </div>
