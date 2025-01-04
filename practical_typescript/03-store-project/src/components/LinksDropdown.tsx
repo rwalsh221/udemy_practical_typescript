2190acb23c4b7f06cf3e273db0121df10671dc87
Richard Walsh <richwalsh2@gmail.com>
2025-01-04 21:31:51 +0000

    section 5 lesson 141

--- practical_typescript/03-store-project/src/components/LinksDropdown.tsx
+++ practical_typescript/03-store-project/src/components/LinksDropdown.tsx
@@ -1,4 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AlignLeft } from "lucide-react";
+import { Button } from "./ui/button";
+import { links } from "@/utils";
+import { NavLink } from "react-router-dom";
+
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle Links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((el) => {
+          return (
+            <DropdownMenuItem key={el.label}>
+              <NavLink
+                to={el.href}
+                className={(isActive) => {
+                  return `capitalize w-full ${isActive ? "text-primary" : ""}`;
+                }}
+              >
+                {el.label}
+              </NavLink>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 export default LinksDropdown;
