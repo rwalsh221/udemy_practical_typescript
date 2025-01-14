330402cd36fac6e72ace8e187acab1c699511342
Richard Walsh <richwalsh2@gmail.com>
2025-01-14 19:38:46 +0000

    section 5 lesson 177

--- practical_typescript/03-store-project/src/utils/pagination.ts
+++ practical_typescript/03-store-project/src/utils/pagination.ts
@@ -9,26 +9,35 @@ export const constructUrl = ({
   pathname,
   search,
 }: ConstructUrlParams): string => {
-  return "/products";
+  const searchParams = new URLSearchParams(search);
+  searchParams.set("page", pageNumber.toString());
+  return `${pathname}?${searchParams.toString()}`;
 };
 
 type ConstructPrevOrNextParams = {
   currentPage: number;
   pageCount: number;
   search: string;
-  pathName: string;
+  pathname: string;
 };
 
-export const ConstructPrevOrNextUrl = ({
+export const constructPrevOrNextUrl = ({
   currentPage,
   pageCount,
   search,
-  pathName,
+  pathname,
 }: ConstructPrevOrNextParams): {
   prevUrl: string;
   nextUrl: string;
 } => {
-  const prevUrl = "products";
-  const nextUrl = "products";
+  let prevPage = currentPage - 1;
+  if (prevPage < 1) prevPage = pageCount;
+  const prevUrl = constructUrl({ pageNumber: prevPage, search, pathname });
+
+  let nextPage = currentPage + 1;
+  if (nextPage > pageCount) nextPage = 1;
+
+  const nextUrl = constructUrl({ pageNumber: nextPage, search, pathname });
+
   return { prevUrl, nextUrl };
 };
