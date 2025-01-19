224758a2a06ad401d29c143851567bcd9dc9845b
Richard Walsh <richwalsh2@gmail.com>
2025-01-19 20:27:19 +0000

    section 5 lesson 193

--- practical_typescript/03-store-project/src/components/CartItemColums.tsx
+++ practical_typescript/03-store-project/src/components/CartItemColums.tsx
@@ -1,5 +1,19 @@
-const CartItemColumns = () => {
-  return <div></div>;
-};
+import { formatAsDollars } from "@/utils";
+import { useAppDispatch } from "@/hooks";
+import { Button } from "./ui/button";
+import { editItem, removeItem } from "@/features/cart/cartSlice";
+import SelectProductAmount from "./SelectProductAmount";
+import { Mode } from "./SelectProductAmount";
 
-export default CartItemColumns;
+export const FirstColumn = () => {
+  return <h4>firstcolumn</h4>;
+};
+export const SecondColumn = () => {
+  return <h4>SecondColumn</h4>;
+};
+export const ThirdColumn = () => {
+  return <h4>ThirdColumn</h4>;
+};
+export const ForthColumn = () => {
+  return <h4>ForthColumn</h4>;
+};
--- practical_typescript/03-store-project/src/components/CartTotals.tsx
+++ practical_typescript/03-store-project/src/components/CartTotals.tsx
@@ -1,5 +1,42 @@
+import { useAppSelector } from "@/hooks";
+import { formatAsDollars } from "@/utils";
+import { Card, CardTitle } from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+
 const CartTotals = () => {
-  return <div></div>;
+  const { cartTotal, shipping, tax, orderTotal } = useAppSelector(
+    (state) => state.cartState
+  );
+  return (
+    <Card className="p-8 bg-muted">
+      <CartTotalRow label="SubTotal" amount={cartTotal} />
+      <CartTotalRow label="shipping" amount={shipping} />
+      <CartTotalRow label="Tax" amount={tax} />
+      <CardTitle className="mt-8">
+        <CartTotalRow label="Order Total" amount={orderTotal} lastRow />
+      </CardTitle>
+    </Card>
+  );
 };
 
 export default CartTotals;
+
+const CartTotalRow = ({
+  label,
+  amount,
+  lastRow,
+}: {
+  label: string;
+  amount: number;
+  lastRow?: boolean;
+}) => {
+  return (
+    <>
+      <p className="flex justify-between text-sm">
+        <span>{label}</span>
+        <span>{formatAsDollars(amount)}</span>
+      </p>
+      {lastRow ? null : <Separator className="my-2" />}
+    </>
+  );
+};
