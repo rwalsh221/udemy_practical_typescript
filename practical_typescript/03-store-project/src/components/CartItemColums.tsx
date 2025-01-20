c9863a14cd4576dff4efae9f2e2e7063466bd3a5
Richard Walsh <richwalsh2@gmail.com>
2025-01-20 20:58:46 +0000

    section 5 lesson195

--- practical_typescript/03-store-project/src/components/CartItemColums.tsx
+++ practical_typescript/03-store-project/src/components/CartItemColums.tsx
@@ -5,15 +5,85 @@ import { editItem, removeItem } from "@/features/cart/cartSlice";
 import SelectProductAmount from "./SelectProductAmount";
 import { Mode } from "./SelectProductAmount";
 
-export const FirstColumn = () => {
-  return <h4>firstcolumn</h4>;
+export const FirstColumn = ({
+  title,
+  image,
+}: {
+  title: string;
+  image: string;
+}) => {
+  return (
+    <img
+      src={image}
+      alt={title}
+      className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover"
+    />
+  );
 };
-export const SecondColumn = () => {
-  return <h4>SecondColumn</h4>;
+
+export const SecondColumn = ({
+  title,
+  company,
+  productColor,
+}: {
+  title: string;
+  company: string;
+  producstColor: string;
+}) => {
+  return (
+    <div className="sm:ml-4 md:ml-12 sm:w-48">
+      <h3 className="capitalize font-medium">{title}</h3>
+      <h4 className="mt-2 capitalize text-sm">{company}</h4>
+      <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
+        color:&nbsp;
+        <span
+          style={{
+            width: "15px",
+            height: "15px",
+            borderRadius: "50%",
+            backgroundColor: productColor,
+          }}
+        ></span>
+      </p>
+    </div>
+  );
 };
-export const ThirdColumn = () => {
-  return <h4>ThirdColumn</h4>;
+
+export const ThirdColumn = ({
+  amount,
+  cartID,
+}: {
+  amount: number;
+  cartID: string;
+}) => {
+  const dispatch = useAppDispatch();
+
+  const removeItemFromCart = () => {
+    dispatch(removeItem(cartID));
+  };
+
+  const setAmount = (value: number) => {
+    dispatch(editItem({ cartID, amount: value }));
+  };
+
+  return (
+    <div>
+      <SelectProductAmount
+        amount={amount}
+        setAmount={setAmount}
+        mode={Mode.CartItem}
+      />
+      <Button
+        variant="link"
+        className="-ml-4 capitalize"
+        onClick={removeItemFromCart}
+      >
+        remove
+      </Button>
+    </div>
+  );
 };
-export const ForthColumn = () => {
-  return <h4>ForthColumn</h4>;
+
+export const ForthColumn = ({ price }: { price: string }) => {
+  return <p className="font-medium sm:ml-auto">{formatAsDollars(price)}</p>;
 };
